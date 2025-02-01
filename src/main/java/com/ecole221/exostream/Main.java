912b0f8b2f10185115c358e10f66a042eee34fe0
bbabadara <bbabadara@gmail.com>
2025-02-01 15:01:46 +0000

    second

--- src/main/java/com/ecole221/exostream/Main.java
+++ src/main/java/com/ecole221/exostream/Main.java
@@ -1,12 +1,15 @@
 package com.ecole221.exostream;
-
 import com.ecole221.exostream.model.Personne;
-
 import java.time.LocalDate;
 import java.time.Period;
 import java.util.Comparator;
 import java.util.List;
+import java.util.Optional;
+import java.util.Collections;
 import java.util.stream.Collectors;
+import java.util.ArrayList;
+
+
 
 
 
@@ -17,10 +20,12 @@ public class Main {
 
         List<Personne> personnes= List.of(
                 new Personne("Fatou","EMP001",LocalDate.of(1998,6,12)),
-                new Personne("Demba","EMP002",LocalDate.of(1960,6,12)),
-                new Personne("Moussa","EMP003",LocalDate.of(1948,6,12)),
-                new Personne("Ndeye","EMP004",LocalDate.of(2000,6,12)),
-                new Personne("Absa","EMP005",LocalDate.of(2018,6,12))
+                new Personne("Demba","EMP002",LocalDate.of(1960,7,1)),
+                new Personne("Moussa","EMP003",LocalDate.of(1948,4,2)),
+                new Personne("Ndeye","EMP004",LocalDate.of(2000,11,30)),
+                new Personne("Absa","EMP005",LocalDate.of(2018,2,15)),
+                new Personne("Baba","EMP006",LocalDate.of(1985,5,18)),
+                new Personne("Alima","EMP006",LocalDate.of(1989,8,14))
         );
 //        System.out.println(personnes);
         // personnes majeures
@@ -49,8 +54,67 @@ public class Main {
 
 //        Calcule l'âge moyen des employés.
 
-//        int ageMoyenne=personnes.stream().reduce((p1, p2) -> Period.between(p1.dateNaissance(),LocalDate.now()).getYears()+Period.between(p2.dateNaissance(),LocalDate.now()).getYears());
-//        System.out.println(messages);
+        double ageMoyen = personnes.stream()
+                .mapToInt(personne -> Period.between(personne.dateNaissance(), LocalDate.now()).getYears())
+                .average()
+                .orElse(0);
+
+
+//        Trouve l'employé le plus âgé.
+//        List<Personne> plusAge = new ArrayList<>(personnes.stream()
+//                .sorted(Comparator.comparingInt(p -> Period.between(p.dateNaissance(), LocalDate.now()).getYears()))
+//                .toList());
+//        Collections.reverse(plusAge);
+
+        Optional<Personne> plusAge = personnes.stream()
+                .max(Comparator.comparingInt(p -> Period.between(p.dateNaissance(), LocalDate.now()).getYears()));
+
+
+//        Trouve l'employé le plus jeune
+        Optional<Personne> plusjeune = personnes.stream()
+                .min(Comparator.comparingInt(p -> Period.between(p.dateNaissance(), LocalDate.now()).getYears()));
+
+//        Regroupe les employés en deux catégories :
+//          1. Moins de 40 ans.
+        List<Personne> moins40 = personnes.stream().filter(personne -> Period.between(personne.dateNaissance(),LocalDate.now()).getYears()<40).toList();
+
+
+//          2. 40 ans et plus.
+        List<Personne> plus40 = personnes.stream().filter(personne -> Period.between(personne.dateNaissance(),LocalDate.now()).getYears()>=40).toList();
+
+//• Vérifie s'il existe au moins un employé ayant moins de 18 ans.
+        boolean verif= personnes.stream().anyMatch(personne -> Period.between(personne.dateNaissance(),LocalDate.now()).getYears()>=18);
+
+
+//        • Vérifie si tous les employés ont un matricule non nul.
+        boolean verif2= personnes.stream().anyMatch(personne -> personne.matricule()=="");
+
+
+//        Pour chaque employé, calcule la date exacte à laquelle il atteindra 60 ans, en ajoutant 60 années à sa dateNaissance.
+        List<String> date60ans= personnes.stream()
+                .map(personne -> "Matricule: "+personne.matricule()+", Nom: "+personne.nom()+", Age: "+Period.between(personne.dateNaissance(),LocalDate.now()).getYears()+", date des 60ans: "+personne.dateNaissance().plusYears(60))
+                .toList();
+
+
+//        calcule de l'age reel
+
+        List<String> ageRelle= personnes.stream()
+                .map(personne -> "Matricule: "+personne.matricule()+", Nom: "+personne.nom()+" Date de naissance: "+personne.dateNaissance()+" Age: "+Period.between(personne.dateNaissance(),LocalDate.now()).getYears()+" ans "+Period.between(personne.dateNaissance(),LocalDate.now()).getMonths()+" mois "+ Period.between(personne.dateNaissance(),LocalDate.now()).getDays()+" jours")
+                .toList();
+
+//        Récupère tous les employés :
+            //• Nés avant l’année 1990.
+        List<Personne> avant1990 = personnes.stream().filter(personne -> personne.dateNaissance().getYear()<1990).toList();
+
+        //• Nés après l’année 2000.
+        List<Personne> apres2000 = personnes.stream().filter(personne -> personne.dateNaissance().getYear()>2000).toList();
+
+        //• Nés durant les années 1980 (entre 1980 et 1989 inclus).
+        List<Personne> entre80 = personnes.stream().filter(personne -> personne.dateNaissance().getYear()>=1980 && personne.dateNaissance().getYear()<=1989 ).toList();
+
+        System.out.println(entre80);
+
+
 
 
     }
